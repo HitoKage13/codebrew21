9a4ce895cc1860374dfaaef95fbd1d0476513d07
Jing Li <jing_li1998@hotmail.com>
2021-04-17 16:28:17 +1000

    Add chakra ui theme

--- next/pages/_app.tsx
+++ next/pages/_app.tsx
@@ -1,9 +1,10 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
+import { theme } from "../theme";
 
 const App = () => {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   );
