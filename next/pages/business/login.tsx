c42d9a0227ef3b662cc8ea5da2a8bb46da6ddb0b
Jing Li <jing_li1998@hotmail.com>
2021-04-17 16:47:52 +1000

    Add business logic layout

--- next/pages/business/login.tsx
+++ next/pages/business/login.tsx
@@ -1,5 +1,23 @@
+import { Box, Text, VStack } from "@chakra-ui/layout";
+import { FormControl, Heading, Input } from "@chakra-ui/react";
+import React from "react";
+
 const Login = () => {
-  return <div></div>;
+  return (
+    <VStack>
+      <Heading>Welcome!</Heading>
+      <Text>Sign in to your business account to continue</Text>
+      <Box as="form">
+        <FormControl>
+          <Input placeholder="Email" />
+        </FormControl>
+        <FormControl>
+          <Input placeholder="Password" />
+        </FormControl>
+        <Text>Don't have an account? Create Account</Text>
+      </Box>
+    </VStack>
+  );
 };
 
 export default Login;
